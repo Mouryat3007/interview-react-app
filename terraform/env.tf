3722bc1da1bcc947d7b2cf1bd4b36db3b03e6ea4
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2026-01-26 18:12:01 +0000

    Create env.tf

---
+++ terraform/env.tf
@@ -0,0 +1,19 @@
+# dev.tfvars
+environment = "dev"
+location    = "eastus"
+sku_tier    = "Free"
+
+# qa.tfvars
+environment = "qa"
+location    = "eastus"
+sku_tier    = "Free"
+
+# stg.tfvars
+environment = "stg"
+location    = "eastus"
+sku_tier    = "Standard"
+
+# prd.tfvars
+environment = "prd"
+location    = "eastus"
+sku_tier    = "Standard"
