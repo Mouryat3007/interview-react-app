6bba1b7c1ef3cd6961450ac481c4b209f0981669
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2026-01-26 23:43:12 +0000

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> DevOps Demo - Interview Project</h1>
-        <h2>Welcome to My Automated CI/CD Pipeline!</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
