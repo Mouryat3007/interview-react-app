3840137faf5fc466932eb6750546c2c1292b950d
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2026-01-26 20:05:20 +0000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,7 @@ terraform {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstateapp"  # Will be overridden per env
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "interview-react-app.tfstate"
   }
 }
 
